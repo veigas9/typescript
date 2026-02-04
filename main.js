de5e2886613aac2e8502f14c3e6d048b5c2a53fd
Andre Veigas <a.luis.veigas@gmail.com>
2026-02-04 14:47:51 -0300

    iniciando repositorio

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multiply = multiply;
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(2, 5));
